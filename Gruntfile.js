52e2351de4054db2781edf96eea4e42433208267
Martin Hecher <martin.hecher@fraunhofer.at>
2014-02-18 17:28:42 +0100

    Adjusted Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function(grunt) {
 
 		watch: {
             scripts: {
-                files: ['src/**/*.js'],
+                files: ['src/**/*.js', 'deps/*.js'],
                 tasks: ['default']
             }
         }
